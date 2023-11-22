bd400842cde0fd37170ce82c7de5c437dd7203c2
matheus silva <matheusgoularten@gmail.com>
2023-11-22 09:21:48 -0300

    Configurando o models "COntact" + Ajustes no painel de admin

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
 
+# id (primary key)
 # Create your models here.
+# first_name (string), last_name (string), phone (string)
+# email (email), created_date (date), description (text)
+# category (foreign key), show (boolean), owner(foreign key)
+# picture (imagem)
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, blank=True)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
+
+
+    def __str__(self) -> str:
+        return f'{self.pk} - {self.first_name} {self.last_name}'
+
