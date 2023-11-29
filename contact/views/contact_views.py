88fe91aff29b467ac1d3adc686cceb863c81a94b
matheus silva <matheusgoularten@gmail.com>
2023-11-29 10:58:54 -0300

    Configurando site tittle e adicionando partials head e header

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -8,6 +8,7 @@ def index(request):
 
     context = {
         'contacts': contacts,
+        'site_title': 'Contatos - '
     }
 
 
@@ -25,9 +26,11 @@ def contact(request, contact_id):
     single_contact = get_object_or_404(
         Contact.objects, pk=contact_id, show = True
         )
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
 
     context = {
         'contact': single_contact,
+        'site_title': site_title,
     }
 
 
